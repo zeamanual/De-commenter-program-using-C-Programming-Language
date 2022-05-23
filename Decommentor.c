0cea3be8d21bd9ff94b2ce35b11fce3b82b9d159
abe <zeamanualfeleke@gmail.com>
2022-05-23 17:12:38 +0300

    decommentor states defined

---
+++ Decommentor.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+enum State{
+    NORMAL_CODE,
+    TO_COMMENT,
+    SINGLE_LINE_COMMENT,
+    MULTIPLE_LINE_COMMENT,
+    TO_END_COMMENT,
+    UNTERMINTATED_COMMENT,
+    DOUBLEQUOTE,
+    SINGLEQUOTE
+};
+
+
+int main(int nArgs, char *Args[]){
+    
+    return 0;
+}
